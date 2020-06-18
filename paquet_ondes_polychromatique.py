fc2e0449cc440d9656f00611bfb9ec51f84b48aa
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2020-06-18 12:47:10 +0200

    Update paquet_ondes_polychromatique.py

--- paquet_ondes_polychromatique.py
+++ paquet_ondes_polychromatique.py
@@ -9,17 +9,19 @@ Created on Thu Jun 18 01:58:45 2020
 from numpy import *
 from matplotlib.pyplot import *
 
-
-w=linspace(1-dw,1+dw,10)
+w=0
 
 def paquet_ondes(dw,t):
-    w=linspace(1-dw,1+dw,100)
+    global w
+    w=arange(1-dw,1+dw,0.001)#pas de linspace pour garder même écart entre deux fréquences
+    #w=linspace(1-dw,1+dw,10)
     a=0
     for _ in w:
-       a=a+sin(_*t)*exp(-t**2/10000)
+       a=a+sin(_*t)
+       #*exp(-t**2/10000)
     return a
-    
-t=linspace(-300,300,1000)
+   
+t=linspace(-1000,1000,4000)
 
 
 f=figure()
@@ -27,15 +29,18 @@ f.suptitle("Paquets d'onde gaussiens $\omega=1$")
 
 g1=subplot(311)
 g1.plot(t,paquet_ondes(0.5,t),"r-")
-g1.text(-100,50,"$d\omega=0.5$")
+g1.text(-100,0,"$d\omega=0.5$")
+g1.set_ylabel("amplitude",rotation=0)
 
 g2=subplot(312)
-g2.plot(t,paquet_ondes(0.05,t),"g-")
-g2.text(-100,50,"$d\omega=0.05$")
+g2.plot(t,paquet_ondes(0.1,t),"g-")
+g2.text(-100,0,"$d\omega=0.05$")
 
 g3=subplot(313)
-g3.plot(t,paquet_ondes(0.005,t),"g-")
-g3.text(-100,50,"$d\omega=0.005$")
+g3.plot(t,paquet_ondes(0.01,t),"g-")
+g3.text(-100,0,"$d\omega=0.005$")
+g3.set_xlabel("temps")
+
 
 show()
 
