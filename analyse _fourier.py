9815aed704b8dc2e5aca7e26ac35499d36788f55
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2020-06-20 12:44:33 +0200

    Update analyse _fourier.py

--- analyse _fourier.py
+++ analyse _fourier.py
@@ -5,6 +5,12 @@ Créé le Thu Jun 18 09:56:12 2020
 
 @auteur: François Gachelin
 
+TF réciproques
+    porte - sinc
+    gaussienne - gaussienne (donc paquets d'onde)
+    
+
+
 """
 
 from numpy import *
@@ -33,32 +39,28 @@ def creneaux(x):
     else:
         return 0
 
-def paquet_ondes(x):
-    df=0.01
-    f=arange(0.1-df,0.1+df,0.0001)
+def paquet_ondes(x):#spectre plat de largeur 2df autour de f avec N ondes monochromatiques
+    f=0.5
+    df=0.05
+    N=1
+    f=linspace(f-df,f+df,N)
     a=0
     for _ in f:
        a=a+sin(2*pi*_*x)
     return a
        
 def sinc(x):
-    return sin(x)/x
-
-def porte_positive(x):
-    if x>0:
-        return 0
-    else:
-        return 1
+    return sin(2*pi*0.4*x)/x
 
 def triangle(x):
-    T=50
+    T=10
     if x%T>T/2:
         return x%T-0.75*T
     else:
         return -x%T-0.75*T
     
 def rampe(x):
-    T=300
+    T=10
     return x%T   
 
 def triangle_simple(x):
@@ -70,8 +72,9 @@ def triangle_simple(x):
     else:
         return T/2-abs(x)
     
-def f(x):
+def cos_carré(x):
     return (cos(0.1*2*pi*x))**2
+
 
 '''
 Paramètres de discrétisation
@@ -87,12 +90,15 @@ dx=2*x_max/N#pas d'échantillonnage
 signal f(x)
 '''
 
+nom_fonction="paquet_ondes"#nom de la fonction sans ()
 x = linspace(-x_max, x_max, N)
-y=[f(_) for _ in x]
+y=[eval(nom_fonction+"(_)") for _ in x]
 
 g1=subplot(121)
-g1.set_title("Nom de la fonction",fontsize=16)
+g1.set_title(nom_fonction.replace("_"," "),fontsize=16)
 g1.plot(x,y)
+g1.set_xlabel("temps $(s)$")
+g1.set_ylabel("Amplitude", rotation=0)
 
 '''
 FFT(s(t))
@@ -108,6 +114,8 @@ g2=subplot(122)
 g2.plot(xf, tf_f,"b-")
 g2.set_title("$DFT$",fontsize=16)
 g2.set_xlim(0.01,1)
+g2.set_xlabel("fréquence $(Hz)$")
+g2.set_ylabel("Amplitude normalisée")
 
 
 show()
